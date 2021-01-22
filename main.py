c5f7f60866e62121ae580eeb43db178a74176e87
Valdecy Pereira <valdecypereira@yahoo.com.br>
2021-01-22 15:37:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,26 +15,26 @@ parameters      = pd.read_csv('VRP-01-Parameters.txt', sep = '\t')
 parameters      = parameters.values
 
 # Parameters - Model
-n_depots    =  2           # The first n rows of the distance_matrix or coordinates
+n_depots    =  1           # The first n Rows of the distance_matrix are Considered as Depots
 time_window = 'without'    # 'with', 'without'
-route       = 'open'       # 'open', 'closed'
+route       = 'closed'     # 'open', 'closed'
 model       = 'vrp'        # 'tsp', 'mtsp', 'vrp'
-graph       = True         # True, False
+graph       = False        # True, False
 
 # Parameters - Vehicle
-vehicle_types = 2          # Two Types of Vehicles: A and B
+vehicle_types = 2          # Quantity of Vehicle Types. In this examples there are 2 Types of Vehicles: A (  or 0) and B ( or 1)
 fixed_cost    = [12 , 25]  # Fixed Cost for Vehicle A = 12; Fixed Cost for Vehicle B = 25
-variable_cost = [0.5,  1]  # Variable Cost for Vehicle A = 0.5; Variable Cost for Vehicle B = 1
+variable_cost = [0.1,  1]  # Variable Cost for Vehicle A = 0.5; Variable Cost for Vehicle B = 1
 capacity      = [9  ,  8]  # Capacity of Vehicle A = 9; Capacity of Vehicle B = 8
 velocity      = [50 , 70]  # Average Velocity of Vehicle A = 50; Average Velocity of Vehicle B = 70. The Average Velocity Value is Used as a Constant that Divides the Distance Matrix.
-fleet_size    = [1  ,  4]  # An Empty List, e.g  fleet_size = [ ], means that the Fleet is Infinite. Non-Empty List, e.g  fleet_size = [15, 7], means that there are available 15 vehicles of type A and 7 vehicles of type B
+fleet_size    = [1  ,  4]  # An Empty List, e.g  fleet_size = [ ], It Means that the Fleet is Infinite. Non-Empty List, e.g  fleet_size = [15, 7], Means that there are available 15 vehicles of type A and 7 vehicles of type B
 
 # Parameters - GA
 penalty_value   = 10000    # GA Target Function Penalty Value for Violating the Problem Constraints
 population_size = 15       # GA Population Size
 mutation_rate   = 0.10     # GA Mutation Rate
 elite           = 1        # GA Elite Member(s) - Total Number of Best Individual(s) that (is)are Maintained 
-generations     = 1000     # GA Number of Generations
+generations     = 2500     # GA Number of Generations
 
 # Call GA Function
 ga_report, ga_vrp = genetic_algorithm_vrp(coordinates, distance_matrix, parameters, velocity, fixed_cost, variable_cost, capacity, population_size, vehicle_types, n_depots, route, model, time_window, fleet_size, mutation_rate, elite, generations, penalty_value, graph)
